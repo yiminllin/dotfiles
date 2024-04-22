1ae06587e38693e401777b1124a430fb061cd8a4
Yimin Lin <yiminlllin@gmail.com>
2024-04-21 22:25:11 -0700

    Telescope - search in current dir

--- nvim/lua/plugins/essential/telescope.lua
+++ nvim/lua/plugins/essential/telescope.lua
@@ -45,6 +45,10 @@ return {
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
+		vim.keymap.set("n", "<leader>sc", function()
+			builtin.find_files({ cwd = require("telescope.utils").buffer_dir() })
+		end, { desc = "[S]earch [C]urrent Directory" })
+
 		vim.keymap.set("n", "<leader>/", function()
 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 				winblend = 10,
