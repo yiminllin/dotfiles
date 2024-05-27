2bad61e42ad227ac911b00eb4b7bb48e93b9f986
Yimin Lin <yiminlllin@gmail.com>
2024-05-27 11:49:55 -0700

    Keymap to resize panes

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -26,3 +26,9 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Use Atl+<hjkl> to set size of splits
+vim.keymap.set("n", "<M-l>", "<c-w>5<")
+vim.keymap.set("n", "<M-h>", "<c-w>5>")
+vim.keymap.set("n", "<M-k>", "<C-W>+")
+vim.keymap.set("n", "<M-j>", "<C-W>-")
