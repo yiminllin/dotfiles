a0141327e7243c8c7d7aa958a7e310fa3c2c2a43
Yimin Lin <yiminlllin@gmail.com>
2024-05-27 03:32:21 -0700

    Floating and dynamic nvim tree

--- nvim/lua/plugins/ui/nvim-tree.lua
+++ nvim/lua/plugins/ui/nvim-tree.lua
@@ -4,7 +4,19 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup({})
+		require("nvim-tree").setup({
+			view = {
+				number = true,
+				width = {
+					min = 30,
+					max = -1,
+					padding = 5,
+				},
+				float = {
+					enable = true,
+				},
+			},
+		})
 		local api = require("nvim-tree.api")
 		local nvimtree_toggle_findfile = function(opt)
 			api.tree.toggle({ path = opt and opt.path, update_root = false, find_file = true, focus = true })
