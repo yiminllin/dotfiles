ebd8501bdf31dce6c8c12d2e3352b2c4e162544e
Yimin Lin <yiminlllin@gmail.com>
2024-05-27 03:39:08 -0700

    Change leap keybinding

--- nvim/lua/plugins/editing/leap.lua
+++ nvim/lua/plugins/editing/leap.lua
@@ -1,7 +1,7 @@
 return {
 	"ggandor/leap.nvim",
 	config = function()
-		require("leap").create_default_mappings()
+		vim.keymap.set("n", "s", "<Plug>(leap)")
 		require("leap").opts.special_keys.prev_target = "<backspace>"
 		require("leap").opts.special_keys.prev_group = "<backspace>"
 		require("leap.user").set_repeat_keys("<enter>", "<backspace>")
