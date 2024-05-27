701a7829f44ba53627b32c71dfa9beb7222e6416
Yimin Lin <yiminlllin@gmail.com>
2024-05-27 03:36:59 -0700

    Add git linker

--- nvim/lua/plugins/editing/which-key.lua
+++ nvim/lua/plugins/editing/which-key.lua
@@ -9,6 +9,7 @@ return {
 		require("which-key").register({
 			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
 			["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+			["<leader>gy"] = { name = "[G]itlink [Y]ank", _ = "which_key_ignore" },
 		})
 	end,
 }
