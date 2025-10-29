28e018d40a3ed58b43e859eb516e8cf6f2e71b1d
Yimin Lin <yiminlllin@gmail.com>
2025-10-29 00:31:55 -0700

    Add tmux status line, use windows instead of session to switch

---
+++ tmux-powerline/.config/tmux-powerline/config.sh
@@ -0,0 +1,10 @@
+source ~/.tmux/plugins/tmux-powerline/config/defaults.sh
+
+# Font color
+export TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR="238"
+export TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR="106"
+export TMUX_POWERLINE_SEG_AIR_COLOR="106"
+
+# Left right status bar
+export TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=("tmux_session_info 223")
+export TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=("pwd 223" "date_day 106" "date 106" "time 106")
