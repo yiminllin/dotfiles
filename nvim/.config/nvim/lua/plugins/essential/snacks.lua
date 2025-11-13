484e0de72f741d819620df88afdadfb919b80bd0
Yimin Lin <yiminlllin@gmail.com>
2025-11-13 02:41:56 -0600

    Use snack picker, keep telescope for dependencies

---
+++ nvim/.config/nvim/lua/plugins/essential/snacks.lua
@@ -0,0 +1,170 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		---------------
+		-- Essential --
+		---------------
+		-- Picker
+		picker = { enabled = true },
+		-- File explorer, picker
+		explorer = { enabled = true },
+		-----------------
+		-- Performance --
+		-----------------
+		-- Prevent LSP and treesitter attaching to big files
+		bigfile = { enabled = true },
+
+		-- Render file before loading plugins
+		quickfile = { enabled = true },
+
+		--------
+		-- UI --
+		--------
+		-- Better vim.ui.input
+		input = { enabled = true },
+		-- Better vim.notify
+		notifier = {
+			enabled = true,
+			timeout = 3000,
+		},
+		styles = {
+			notification = {
+				wo = { wrap = true }, -- Wrap notifications
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>sh",
+			function()
+				Snacks.picker.help()
+			end,
+			desc = "[S]earch [H]elp",
+		},
+		{
+			"<leader>sk",
+			function()
+				Snacks.picker.keymaps()
+			end,
+			desc = "[S]earch [K]eymaps",
+		},
+		{
+			"<leader>sf",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "[S]earch [F]iles",
+		},
+		{
+			"<leader>sw",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "[S]earch current [W]ord",
+			mode = { "n", "x" },
+		},
+		{
+			"<leader>sb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "[S]earch Open [B]uffers",
+		},
+		{
+			"<leader>sg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "[S]earch by [G]rep",
+		},
+		{
+			"<leader>e",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "File [E]xplorer",
+		},
+		{
+			"<leader>/",
+			function()
+				Snacks.picker.lines()
+			end,
+			desc = "[/] Fuzzily search in current buffer",
+		},
+		{
+			"<leader>sn",
+			function()
+				Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "[S]earch [N]eovim Config File",
+		},
+		{
+			"<leader><leader>",
+			function()
+				Snacks.picker.git_status()
+			end,
+			desc = "Git Status",
+		},
+		{
+			"<leader>sm",
+			function()
+				Snacks.picker.marks()
+			end,
+			desc = "[S]earch [M]arks",
+		},
+		-- LSP
+		{
+			"gd",
+			function()
+				Snacks.picker.lsp_definitions()
+			end,
+			desc = "[G]oto [D]efinition",
+		},
+		{
+			"gD",
+			function()
+				Snacks.picker.lsp_declarations()
+			end,
+			desc = "[G]oto [D]eclaration",
+		},
+		{
+			"gr",
+			function()
+				Snacks.picker.lsp_references()
+			end,
+			nowait = true,
+			desc = "[G]oto [R]eferences",
+		},
+		{
+			"gI",
+			function()
+				Snacks.picker.lsp_implementations()
+			end,
+			desc = "[G]oto [I]mplementation",
+		},
+		{
+			"gt",
+			function()
+				Snacks.picker.lsp_type_definitions()
+			end,
+			desc = "[G]oto [T]ype Definition",
+		},
+		{
+			"gai",
+			function()
+				Snacks.picker.lsp_incoming_calls()
+			end,
+			desc = "[G]oto C[a]lls [I]ncoming",
+		},
+		{
+			"gao",
+			function()
+				Snacks.picker.lsp_outgoing_calls()
+			end,
+			desc = "[G]oto C[a]lls [O]utgoing",
+		},
+	},
+}
--- nvim/.config/nvim/lua/plugins/essential/telescope.lua
+++ nvim/.config/nvim/lua/plugins/essential/telescope.lua
@@ -42,47 +42,47 @@ return {
 		pcall(require("telescope").load_extension, "fzf")
 		pcall(require("telescope").load_extension, "ui-select")
 
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>sf", function()
-			builtin.find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } })
-		end, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", function()
-			builtin.live_grep({
-				additional_args = function()
-					return { "--hidden", "--glob", "!.git/*" }
-				end,
-			})
-		end, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [B]uffers" })
-		vim.keymap.set("n", "<leader><leader>", builtin.git_status, { desc = "[S]earch [G]it status" })
-
-		vim.keymap.set("n", "<leader>sc", function()
-			builtin.find_files({ cwd = require("telescope.utils").buffer_dir() })
-		end, { desc = "[S]earch [C]urrent Directory" })
-
-		vim.keymap.set("n", "<leader>/", function()
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
-
-		vim.keymap.set("n", "<leader>s/", function()
-			builtin.live_grep({
-				grep_open_files = true,
-				prompt_title = "Live Grep in Open Files",
-			})
-		end, { desc = "[S]earch [/] in Open Files" })
-
-		vim.keymap.set("n", "<leader>sn", function()
-			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
+		-- local builtin = require("telescope.builtin")
+		-- vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+		-- vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+		-- vim.keymap.set("n", "<leader>sf", function()
+		-- 	builtin.find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } })
+		-- end, { desc = "[S]earch [F]iles" })
+		-- vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+		-- vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+		-- vim.keymap.set("n", "<leader>sg", function()
+		-- 	builtin.live_grep({
+		-- 		additional_args = function()
+		-- 			return { "--hidden", "--glob", "!.git/*" }
+		-- 		end,
+		-- 	})
+		-- end, { desc = "[S]earch by [G]rep" })
+		-- vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+		-- vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+		-- vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+		-- vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [B]uffers" })
+		-- vim.keymap.set("n", "<leader><leader>", builtin.git_status, { desc = "[S]earch [G]it status" })
+		--
+		-- vim.keymap.set("n", "<leader>sc", function()
+		-- 	builtin.find_files({ cwd = require("telescope.utils").buffer_dir() })
+		-- end, { desc = "[S]earch [C]urrent Directory" })
+		--
+		-- vim.keymap.set("n", "<leader>/", function()
+		-- 	builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		-- 		winblend = 10,
+		-- 		previewer = false,
+		-- 	}))
+		-- end, { desc = "[/] Fuzzily search in current buffer" })
+		--
+		-- vim.keymap.set("n", "<leader>s/", function()
+		-- 	builtin.live_grep({
+		-- 		grep_open_files = true,
+		-- 		prompt_title = "Live Grep in Open Files",
+		-- 	})
+		-- end, { desc = "[S]earch [/] in Open Files" })
+		--
+		-- vim.keymap.set("n", "<leader>sn", function()
+		-- 	builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		-- end, { desc = "[S]earch [N]eovim files" })
 	end,
 }
