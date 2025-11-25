274debc531946cd760f79fddc7c234e127befb5a
Yimin Lin <yiminlllin@gmail.com>
2025-11-25 00:12:07 -0800

    Add venn mode lualine

--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -2,9 +2,20 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local function venn_mode()
+			if vim.b.venn_enabled then
+				return "VENN"
+			else
+				return ""
+			end
+		end
 		require("lualine").setup({
 			options = { theme = "gruvbox" },
 			sections = {
+				lualine_a = {
+					"mode",
+					venn_mode,
+				},
 				lualine_c = {
 					{ "filename", path = 1 },
 				},
