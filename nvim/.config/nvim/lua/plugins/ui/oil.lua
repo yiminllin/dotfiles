f08924f26b1833cdece45733a82df01ff9cf811f
Yimin Lin <yiminlllin@gmail.com>
2025-11-26 20:33:12 -0800

    Oil, kepmap for yank path

--- nvim/.config/nvim/lua/plugins/ui/oil.lua
+++ nvim/.config/nvim/lua/plugins/ui/oil.lua
@@ -12,6 +12,12 @@ return {
 				border = "rounded",
 			},
 		})
-		vim.keymap.set("n", "<leader>o", require("oil").toggle_float, { desc = "[O]pen Parent Directory" })
+		vim.keymap.set("n", "<leader>o<leader>", require("oil").toggle_float, { desc = "[O]pen Parent Directory" })
+		vim.keymap.set(
+			"n",
+			"<leader>oy",
+			require("oil.actions").yank_entry.callback,
+			{ desc = "[O]pen Parent Directory, and [Y]ank entry" }
+		)
 	end,
 }
