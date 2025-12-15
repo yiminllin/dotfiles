50680e4ad50f36085ba1a6b406c36cd1fa87ea4c
Yimin Lin <yiminlllin@gmail.com>
2025-12-15 05:12:15 +0000

    Fix treesitter nvim

--- nvim/.config/nvim/lua/plugins/essential/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/essential/treesitter.lua
@@ -1,25 +1,22 @@
 return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		---@diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"html",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"vim",
-				"vimdoc",
-				"diff",
-				"git_config",
-			},
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+	opts = {
+		ensure_installed = {
+			"bash",
+			"c",
+			"cpp",
+			"html",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"vim",
+			"vimdoc",
+			"diff",
+			"git_config",
+		},
+		auto_install = true,
+		highlight = { enable = true },
+		indent = { enable = true },
+	},
 }
