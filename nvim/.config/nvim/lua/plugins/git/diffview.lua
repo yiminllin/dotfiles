6930b003f7180cc3ccc3139ed4397aed83c91d42
Yimin Lin <yiminlllin@gmail.com>
2025-12-26 13:35:13 -0800

    Use solarized for gitdiff

--- nvim/.config/nvim/lua/plugins/git/diffview.lua
+++ nvim/.config/nvim/lua/plugins/git/diffview.lua
@@ -1,8 +1,50 @@
 return {
 	"sindrets/diffview.nvim",
+	dependencies = {
+		{ "lifepillar/vim-solarized8", branch = "neovim" }, -- Pin to master branch
+	},
 	config = function()
 		require("diffview").setup({
 			enhanced_diff_hl = true,
+		})
+
+		local refresh_ibl = function()
+			vim.api.nvim_set_hl(0, "Base2", { fg = "#eee8d5" })
+			require("ibl").setup({
+				indent = { highlight = "Base2", repeat_linebreak = true },
+				scope = { show_start = false, show_end = false },
+			})
+		end
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "DiffviewViewOpened",
+			callback = function()
+				vim.cmd("DiffviewToggleFiles")
+				vim.o.background = "light"
+				vim.cmd.colorscheme("solarized8_flat")
+				vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#e6e9c1" })
+				vim.api.nvim_set_hl(0, "DiffChange", { bg = "#cecba1" })
+				vim.api.nvim_set_hl(0, "DiffText", { bg = "#c5e0dc", fg = "#323024", bold = true })
+				vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#f4c2a2" })
+				refresh_ibl()
+			end,
+		})
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "DiffviewViewClosed",
+			callback = function()
+				require("gruvbox").setup({
+					contrast = "hard",
+					overrides = {
+						DiffDelete = { bg = "#f4c2a2" },
+						DiffAdd = { bg = "#e6e9c1" },
+						DiffChange = { bg = "#cecba1" },
+						DiffText = { bg = "#c5e0dc", fg = "#323024", bold = true },
+					},
+				})
+				vim.o.background = "light"
+				vim.cmd.colorscheme("gruvbox")
+				refresh_ibl()
+			end,
 		})
 	end,
 	keys = {
--- nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,12 +1,4 @@
 return {
-	-- -- Solarized color scheme
-	-- "lifepillar/vim-solarized8",
-	-- lazy = false,
-	-- priority = 1000,
-	-- config = function()
-	-- 	vim.o.background = "light"
-	-- 	vim.cmd.colorscheme("solarized8_flat")
-	-- end,
 	-- Gruvbox color scheme
 	"ellisonleao/gruvbox.nvim",
 	lazy = false,
