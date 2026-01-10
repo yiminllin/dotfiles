113c7dc1003f17072d805bbe6b606519a1601859
Yimin Lin <yiminlllin@gmail.com>
2026-01-09 21:38:34 -0800

    Add git branch in tmux powerline

--- tmux-powerline/.config/tmux-powerline/config.sh
+++ tmux-powerline/.config/tmux-powerline/config.sh
@@ -7,5 +7,5 @@ export TMUX_POWERLINE_SEG_AIR_COLOR="106"
 
 # Left right status bar
 # Cannot export array in bash: define as global variables
-TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=("tmux_session_info 223")
+TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=("tmux_session_info 223" "vcs_branch 244 223")
 TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=("pwd 223" "date_day 106" "date 106" "time 106")
