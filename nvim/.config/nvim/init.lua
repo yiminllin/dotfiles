d2b509ea9feb2778c1862665b9283578f1e6dbf1
Yimin Lin <yiminlllin@gmail.com>
2026-01-31 23:21:00 -0800

    Minimal config for kitty-scrollback, for performance

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,19 @@
+-- kitty-scrollback.nvim: minimal config (kitten sets KITTY_SCROLLBACK_NVIM=true)
+if vim.env.KITTY_SCROLLBACK_NVIM == "true" then
+	require("settings")
+	-- Apply gruvbox + visual highlight when scrollback buffer is ready (FileType fires then)
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "kitty-scrollback",
+		callback = function()
+			vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/gruvbox.nvim")
+			vim.cmd.colorscheme("gruvbox")
+			vim.api.nvim_set_hl(0, "KittyScrollbackNvimVisual", { link = "Visual" })
+			vim.api.nvim_set_hl(0, "KittyScrollbackNvimNormal", { link = "Normal" })
+		end,
+	})
+	return
+end
+
 require("settings")
 require("keymaps")
 require("autocmds")
