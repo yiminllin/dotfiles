da13e0b35f820529b6c16651c40005cb730489f5
Yimin Lin <yiminlllin@gmail.com>
2026-03-22 17:07:57 +0000

    Fix nvim luarocks path

--- nvim/.config/nvim/lua/plugins/essential/luarocks.lua
+++ nvim/.config/nvim/lua/plugins/essential/luarocks.lua
@@ -1,5 +1,13 @@
 return {
-	"vhyrro/luarocks.nvim",
-	priority = 1000,
-	config = true,
+  "vhyrro/luarocks.nvim",
+  priority = 1000,
+  config = function(plugin, opts)
+    local vendor_path = plugin.dir .. "/.rocks/share/lua/5.1/luarocks/vendor/?.lua"
+
+    if not string.find(package.path, vendor_path, 1, true) then
+      package.path = vendor_path .. ";" .. package.path
+    end
+
+    require("luarocks-nvim").setup(opts)
+  end,
 }
