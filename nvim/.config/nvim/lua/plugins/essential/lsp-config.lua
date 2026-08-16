0540db0fc473df16c9b3f85f8270612a39054261
Yimin Lin <yiminlllin@gmail.com>
2026-08-16 15:07:11 -0500

    Remove rustfmt in lsp config

--- nvim/.config/nvim/lua/plugins/essential/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/essential/lsp-config.lua
@@ -85,7 +85,6 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua",
-			"rustfmt",
 			"prettier",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
