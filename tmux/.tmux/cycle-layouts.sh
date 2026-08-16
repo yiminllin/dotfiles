8e282260e6246275eec0793da95c150d026c44ba
Yimin Lin <yiminlllin@gmail.com>
2026-08-16 14:45:20 -0500

    Resolve compatibility issues on macos for tmux agent board and sessionizer

--- tmux/.tmux/cycle-layouts.sh
+++ tmux/.tmux/cycle-layouts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Cycle through layouts for left/right panes
 
-separator=$'\034'
+separator='|'
 original_pane_id=$(tmux display-message -p "#{pane_id}")
 
 target_pane=""
@@ -14,7 +14,7 @@ while IFS="$separator" read -r pane_top pane_left pane_id board_flag; do
     target_pane=$pane_id
     target_top=$pane_top
     target_left=$pane_left
-  elif (( pane_top > target_top || (pane_top == target_top && pane_left < target_left) )); then
+  elif ((pane_top > target_top || (pane_top == target_top && pane_left < target_left))); then
     target_pane=$pane_id
     target_top=$pane_top
     target_left=$pane_left
@@ -29,15 +29,15 @@ pane_width=$(tmux display-message -p -t "$target_pane" "#{pane_width}")
 # Categorize the window width as
 #     w0  w1  w2
 # |   |   |   |   |
-w0=$(( 3*window_width / 10 ))
-w1=$(( 5*window_width / 10 ))
-w2=$(( 7*window_width / 10 ))
-pane_center_distance=$(( pane_width - w1 ))
-pane_center_abs_distance=${pane_center_distance#-}  # Absoluate value by str manipulation
+w0=$((3 * window_width / 10))
+w1=$((5 * window_width / 10))
+w2=$((7 * window_width / 10))
+pane_center_distance=$((pane_width - w1))
+pane_center_abs_distance=${pane_center_distance#-} # Absoluate value by str manipulation
 
-if (( pane_center_abs_distance <= 1 )); then
+if ((pane_center_abs_distance <= 1)); then
   tmux resize-pane -t "$target_pane" -x $w2
-elif (( $pane_width <= $w1 )); then
+elif (($pane_width <= $w1)); then
   tmux resize-pane -t "$target_pane" -x $w1
 else
   tmux resize-pane -t "$target_pane" -x $w0
